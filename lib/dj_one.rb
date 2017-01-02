101b293b93c8e866183215e14a3731fc1e50eb41
Piotr Jakubowski <piotrj@gmail.com>
2017-01-02 19:16:59 +0100

    Implementation for processing_id and handling failures
    When the job goes into processing we want to be able to schedule another
    one of the same kind, so that we always finish up with the job with the
    most current state.
    
    Also when the job fails for the last time we need to clear the unique_id
    so that another instances of that job can take over.

--- lib/dj_one.rb
+++ lib/dj_one.rb
@@ -15,13 +15,42 @@ module DjOne
     class Handler
       def initialize(lifecycle)
         lifecycle.around(:enqueue, &method(:enqueue))
+        lifecycle.around(:perform, &method(:perform))
+        lifecycle.around(:failure, &method(:failure))
       end
 
       def enqueue(job, &proceed)
-        job_handler = job.payload_object
-        job.unique_id = job_handler.respond_to?(:unique_id) &&  job_handler.unique_id
+        job.unique_id = get_attribute(job, :enqueue_id)
         proceed.call
       rescue ActiveRecord::RecordNotUnique
+      end
+
+      def perform(worker, job, &proceed)
+        job.unique_id = get_attribute(job, :perform_id)
+        job.save! if job.changed?
+
+        proceed.call
+      rescue ActiveRecord::RecordNotUnique
+        job.unique_id = job.unique_id_was
+        job.run_at = calculate_run_at(job)
+        job.save! if job.changed?
+      end
+
+      def failure(worker, job, &proceed)
+        job.unique_id = nil
+        proceed.call
+      end
+
+      def get_attribute(job, method_name)
+        object = job.payload_object
+        object.respond_to?(method_name) ? object.public_send(method_name) : nil
+      rescue Delayed::DeserializationError
+        nil
+      end
+
+      def calculate_run_at(job)
+        delay = get_attribute(job, :duplicate_delay) || DEFAULT_DUPLICATE_DELAY
+        Time.now + delay
       end
     end
   end
--- test/dj_one_test.rb
+++ test/dj_one_test.rb
@@ -3,15 +3,29 @@ require 'test_helper'
 class DjOneTest < Minitest::Test
   class TestJob < Struct.new(:user_id)
     def perform
+      puts "A"
     end
 
-    def unique_id
-      "test_job_#{user_id}"
+    def enqueue_id
+      "test_job_#{user_id}_enqueued"
+    end
+
+    def perform_id
+      "test_job_#{user_id}_performing"
+    end
+
+    def max_attempts
+      2
+    end
+
+    def destroy_failed_jobs?
+      false
     end
   end
 
   def setup
     clear_jobs
+    clear_workers
   end
 
   def test_without_dj_one_you_can_schedule_two_identical_jobs
@@ -24,10 +38,10 @@ class DjOneTest < Minitest::Test
 
     refute_nil job1.id
     refute_nil job2.id
-    assert_equal Delayed::Job.count, 2
+    assert_equal 2, Delayed::Job.count
   end
 
-  def test_with_dj_one_it_prevents_scheduling_same_job_twice
+  def test_it_prevents_scheduling_same_job_twice
     with_dj_one
 
     user_id = 14
@@ -37,11 +51,71 @@ class DjOneTest < Minitest::Test
 
     refute_nil job1.id
     assert_nil job2.id
-    assert_equal Delayed::Job.count, 1
+    assert_equal 1, Delayed::Job.count
+  end
+
+  def test_job_changes_unique_id_when_it_is_being_processed
+    with_dj_one
+
+    user_id = 16
+    job1 = schedule_test_job(user_id)
+
+    job1_scheduled_id = job1.unique_id
+    job1_performing_id = nil
+    stub_perform do
+      job1_performing_id = job1.reload.unique_id
+    end
+
+    Delayed::Worker.new.work_off
+    assert_match /enqueued$/, job1_scheduled_id
+    assert_match /performing$/, job1_performing_id
+  end
+
+  def test_same_job_cannot_be_processed_at_the_same_time
+    with_dj_one
+
+    user_id = 16
+    job1 = schedule_test_job(user_id)
+    mark_as_processing(job1)
+
+    job2 = schedule_test_job(user_id)
+
+    job_processed = false
+    stub_perform do
+      job_processed = true
+    end
+
+    Delayed::Worker.new.work_off
+
+    assert_equal false, job_processed
+  end
+
+  def test_when_job_fails_for_the_last_time_unique_id_is_removed
+    with_dj_one
+
+    user_id = 16
+    job1 = schedule_test_job(user_id)
+
+    stub_perform do
+      raise StandardError.new
+    end
+
+    Delayed::Worker.new.work_off
+
+    refute_nil job1.reload.unique_id
+    job1.update_attributes(run_at: Time.now)
+
+    Delayed::Worker.new.work_off
+
+    assert_nil job1.reload.unique_id
   end
 
   def clear_jobs
     Delayed::Job.delete_all
+  end
+
+  def clear_workers
+    @workers_count = 0
   end
 
   def without_dj_one
@@ -56,5 +130,25 @@ class DjOneTest < Minitest::Test
 
   def schedule_test_job(user_id)
     Delayed::Job.enqueue(TestJob.new(user_id))
+  end
+
+  def stub_perform(&block)
+    TestJob.send(:define_method, :perform_stub, &block)
+    TestJob.send(:alias_method, :perform_original, :perform)
+    TestJob.send(:alias_method, :perform, :perform_stub)
+  ensure
+
+  end
+
+  def clear_perform_stub
+    if TestJob.new.respond_to?(:perform_original)
+      TestJob.send(:undef_method, :perform_stub)
+      TestJob.send(:alias_method, :perform, :perform_original)
+      TestJob.send(:undef_method, :perform_original)
+    end
+  end
+
+  def mark_as_processing(job)
+    job.update_attributes(unique_id: job.payload_object.enqueue_id, locked_at: Time.now)
   end
 end
